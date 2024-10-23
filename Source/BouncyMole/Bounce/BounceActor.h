9686ca77042929e55d7486275b1b8cca1a1d7df3
mac2648 <60781347+mac2648@users.noreply.github.com>
2024-10-23 16:45:16 -0400

    created a basic version of the bounce wall

---
+++ Source/BouncyMole/Bounce/BounceActor.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BounceActor.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class BOUNCYMOLE_API ABounceActor : public AActor
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* Collider;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* Mesh;
+
+	//1.0 is no multiplier
+	UPROPERTY(EditAnywhere)
+	float SpeedMultiplier = 1.2f;
+
+	UPROPERTY(EditAnywhere)
+	bool InvertX = false;
+
+	UPROPERTY(EditAnywhere)
+	bool InvertY = false;
+
+public:	
+	// Sets default values for this actor's properties
+	ABounceActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void Bounce(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	void Bounce(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	float GetAngleFromNormalizedVector(const FVector2D& Vec);
+};
