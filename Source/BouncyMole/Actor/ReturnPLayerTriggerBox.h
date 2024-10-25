54bad2e6d9d9b4d7859ccf8297015e90861497e4
mac2648 <60781347+mac2648@users.noreply.github.com>
2024-10-24 23:32:21 -0400

    Created actor

---
+++ Source/BouncyMole/Actor/ReturnPLayerTriggerBox.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ReturnPLayerTriggerBox.generated.h"
+
+class UBoxComponent;
+class ACharacter;
+
+UCLASS()
+class BOUNCYMOLE_API AReturnPLayerTriggerBox : public AActor
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* Box;
+
+	FVector PlayerStartPosition;
+	
+public:	
+	// Sets default values for this actor's properties
+	AReturnPLayerTriggerBox();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void MovePlayerBack(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
