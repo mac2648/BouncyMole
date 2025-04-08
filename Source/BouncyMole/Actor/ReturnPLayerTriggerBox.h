398d56197b78a2c1c77ec90d5a0f755a147b1972
mac2648 <60781347+mac2648@users.noreply.github.com>
2025-04-08 17:26:06 -0400

    added a widget to tell the player they were out of the map

--- Source/BouncyMole/Actor/ReturnPLayerTriggerBox.h
+++ Source/BouncyMole/Actor/ReturnPLayerTriggerBox.h
@@ -17,6 +17,12 @@ class BOUNCYMOLE_API AReturnPLayerTriggerBox : public AActor
 	UPROPERTY(EditAnywhere)
 	UBoxComponent* Box;
 
+	UPROPERTY()
+	UUserWidget* MessageWidget = nullptr;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UUserWidget> WidgetClass;
+
 	FVector PlayerStartPosition;
 	
 public:	
@@ -29,6 +35,9 @@ protected:
 
 	UFUNCTION()
 	void MovePlayerBack(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void HideWidget();
 
 public:	
 	// Called every frame
